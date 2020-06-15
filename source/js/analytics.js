590221d8ee13c2d9ad569727699e7e8b00d03d7c
yunpeng.zhan <136688059@qq.com>
2020-06-15 22:36:41 +0800

    update:add analytics

---
+++ source/js/analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c0857228b0994468152e5695eefe7822";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
